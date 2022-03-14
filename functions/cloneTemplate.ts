b002edb9c1c3ad5b647c23362ff1f9053b2929e1
Avi Avinav <aviavinav2004@gmail.com>
2022-03-14 22:07:48 +0530

    init cloneTemplate function

---
+++ functions/cloneTemplate.ts
@@ -0,0 +1,15 @@
+// import shell from 'shelljs';
+
+// const SKIP_FILES = [
+//   '.git',
+//   'node_modules',
+//   'yarn.lock',
+//   'package-lock.json',
+//   'pnpm-lock.yaml',
+// ];
+
+// const WORKING_DIR = process.cwd();
+
+// const cloneTemplate = (template: string, name: string) => {
+
+// }
