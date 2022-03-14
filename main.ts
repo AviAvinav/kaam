8f37ce9eb2e41f144a6f06fa2ebc5c35652406e8
Avi Avinav <aviavinav2004@gmail.com>
2022-03-14 21:25:38 +0530

    add questions

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-console.log('Hello World');
+#!/usr/bin/env node
+
+import * as inquirer from 'inquirer';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
+
+const QUESTIONS = [
+  {
+    name: 'template',
+    type: 'list',
+    message: 'What project would you like to generate?',
+    choices: CHOICES,
+  },
+  {
+    name: 'name',
+    type: 'input',
+    message: 'Enter a name for your project: ',
+    default: 'new-project',
+  },
+  {
+    name: 'license',
+    type: 'list',
+    message: 'Choose a license: ',
+    choices: ['MIT', 'ISC', 'Apache', 'GPLv3', 'BSD', 'None'],
+  },
+  {
+    name: 'prettier',
+    type: 'list',
+    message: 'Would you like to use Prettier?',
+    choices: ['Yes', 'No'],
+  },
+  {
+    name: 'pkg-manager',
+    type: 'list',
+    message: 'Choose a package manager: ',
+    choices: ['yarn', 'npm', 'pnpm'],
+  },
+];
+
+inquirer.prompt(QUESTIONS).then((answers) => {
+  console.log(answers);
+});
