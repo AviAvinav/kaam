e46e174be0ba6e01faeae2d309b35967a1aaeff9
Avi Avinav <aviavinav2004@gmail.com>
2022-03-16 16:54:30 +0530

    remove line space

--- functions/cloneTemplate.ts
+++ functions/cloneTemplate.ts
@@ -10,7 +10,7 @@ const cloneTemplate = async (template: string, name: string, pkgManager: string)
   try {
     shell.exec(`cd ${name} && npx degit github:AviAvinav/kaam/templates/${template} && ${pkgManager} install`, { silent: true });
     await new Promise((resolve) => { setTimeout(resolve, 1000); });
-    spinner.success({ text: '\nProject generated successfully! 🚀 🚀 🚀' });
+    spinner.success({ text: 'Project generated successfully! 🚀 🚀 🚀' });
     console.log(chalk.green(`\nI have already run '${pkgManager} install' for you, so go ahead and dive in.`));
   } catch (err) {
     spinner.error({ text: `${err}` });
