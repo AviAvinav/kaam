734d26047bfa3ef5d8a51f49bf2f71524bcdb20d
Avi Avinav <aviavinav2004@gmail.com>
2022-10-21 01:27:45 +0530

    feat(createDirectory): add option to rerun on fail
    chalk started throwing errors

--- src/functions/createDirectory.ts
+++ src/functions/createDirectory.ts
@@ -1,17 +1,39 @@
 import * as fs from "fs";
 import * as chalk from "chalk";
+import * as inquirer from "inquirer";
 
 const createDirectory = (targetPath: string) => {
+  let rerun = false;
   if (fs.existsSync(targetPath)) {
     console.log(
       chalk.redBright(
         "A directory with the same name as the project already exists. Please use another name or delete the directory."
       )
     );
-    return false;
+
+    inquirer
+      .prompt([
+        {
+          name: "rerun",
+          type: "confirm",
+          message: "Would you like to retry?",
+          choices: ["yes", "no"],
+        },
+      ])
+      .then((answers) => {
+        rerun = answers["rerun"] === "yes" ? true : false;
+      });
+    return {
+      success: false,
+      rerun,
+    };
   }
+
   fs.mkdirSync(targetPath);
-  return true;
+  return {
+    success: true,
+    rerun,
+  };
 };
 
 export default createDirectory;
